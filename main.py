cd6ce03e5b41844ed6adee98335f272059d3dd01
kristoman-rikardo <kristofferswik@gmail.com>
2025-03-11 21:17:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def crawl(url: str = Query(..., title="URL to scrape")):
     try:
         result = await crawler.arun(
             url=url,
-            wait_until="networkidle",
+            wait_until="load",
             goto_options={"timeout": 1}  # 1,5 sekunder
         )
         return {"content": result.markdown}
